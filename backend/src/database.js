802cb6a19f33e8ac54d977c268e8f8b9f8290d0b
shecho <shechovala@gmail.com>
2020-01-14 15:58:29 -0500

    final cheack

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/courseTM', {
+mongoose
+  .connect("mongodb://localhost/courseTM", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('Connected to database'));
+  })
+  .then(() => console.log("Connected to database"));
