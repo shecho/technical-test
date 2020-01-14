8d196d7ff64a52ecaa0cfd8dd1aaeeba79eec0b5
shecho <shechovala@gmail.com>
2020-01-14 09:35:44 -0500

    added bootstrap, pagination skel (no working yet)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-md-4" v-for="course in displayed" :key="course._id">
+          <div class="card mb-2 box-shadow post-cards">
+            <img v-bind:src="course.imageURL" class="card-img-top" />
+            <div class="card-body">
+              <h5 class="capitalize">{{ course.name }}</h5>
+              <p class="card-text">{{ course.description.slice(0, 120) }}...</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--     Loop through the pages array to display each page number(still not working propertly)       -->
+      <div class="btn-group col-md-2 offset-md-5">
+        <button
+          type="button"
+          class="btn btn-sm btn-outline-secondary"
+          v-if="page != 1"
+          @click="page--"
+        ></button>
+        <button
+          type="button"
+          class="btn btn-sm btn-outline-secondary"
+          v-for="pageNumber in pages.slice(page - 1, page + 5)"
+          @click="page = pageNumber"
+          :key="pageNumber"
+        >{{ pageNumber }}</button>
+        <button
+          type="button"
+          @click="page++"
+          v-if="page < pages.length"
+          class="btn btn-sm btn-outline-secondary"
+        >>></button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      courses: [],
+      baseURL: "http://localhost:3000",
+      page: 1,
+      perPage: 10,
+      pages: []
+    };
+  },
+  created() {
+    this.getPosts();
+  },
+  methods: {
+    async getPosts() {
+      const res = await this.axios.get(`${this.baseURL}/courses`);
+      this.courses = res.data.courses;
+    },
+    setCourses() {
+      let numberOfPages = Math.ceil(this.courses.length / this.perPage);
+      for (let i = 1; i <= numberOfPages; i++) {
+        this.pages.push(i);
+      }
+    },
+    paginate(courses) {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = page * perPage - perPage;
+      let to = page * perPage;
+      return courses.slice(from, to);
+    }
+  },
+  watch: {
+    courses() {
+      this.setCourses();
+    }
+  },
+  computed: {
+    displayedCourses: function() {
+      return this.paginate(this.courses);
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
